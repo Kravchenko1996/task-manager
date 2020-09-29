1bb3c7776f6d6c0f5ba02a20fed9f851fd0fa774
kravoleksii <kravoleksii@gmail.com>
2020-09-29 21:14:34 +0300

    -fix view;

--- rubygarage_task_manager/urls.py
+++ rubygarage_task_manager/urls.py
@@ -2,15 +2,9 @@ from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path, include
 
-
-def index(request):
-    return render(request, 'index.html')
-
-
 urlpatterns = [
-    path('', index),
+    path('', lambda x: render(x, 'index.html')),
     path('admin/', admin.site.urls),
     path('api/v1beta/', include('apps.task_manager.api.urls')),
     path('api/v2beta/', include('apps.users.api.urls'))
 ]
-
