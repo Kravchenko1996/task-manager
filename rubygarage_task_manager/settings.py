35f0aa2d01f08666ce18ca0d494bb3123fc9844f
kravoleksii <kravoleksii@gmail.com>
2020-09-29 17:26:16 +0300

    -updated settings.py;

--- rubygarage_task_manager/settings.py
+++ rubygarage_task_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import datetime
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,3 +142,5 @@ JWT_AUTH = {
     'JWT_ALLOW_REFRESH': True,
     'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300)
 }
+
+django_heroku.settings(locals())
