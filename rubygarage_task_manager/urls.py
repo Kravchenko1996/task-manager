18259cf8c482ac7f8427f1d4cbd0807b29840288
kravoleksii <kravoleksii@gmail.com>
2020-09-29 21:10:18 +0300

    -connected Angular with Django;

--- rubygarage_task_manager/urls.py
+++ rubygarage_task_manager/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
 
+
+def index(request):
+    return render(request, 'index.html')
+
+
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('api/v1beta/', include('apps.task_manager.api.urls')),
     path('api/v2beta/', include('apps.users.api.urls'))
 ]
+
