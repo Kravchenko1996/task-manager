5f90d9e59c2f16084ef54b1b7b2f19d96f7fb250
kravoleksii <kravoleksii@gmail.com>
2020-09-29 18:16:25 +0300

    -updated settings.py;

--- rubygarage_task_manager/settings.py
+++ rubygarage_task_manager/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qbr4mdjk*of%45zypq3c)dtp+75^(1(x64*l_$ae8!2%q6%qxf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['rubygarage-task-manager.herokuapp.com']
 
